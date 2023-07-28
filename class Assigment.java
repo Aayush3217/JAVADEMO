5538fb78511949ce19e0d612682ab4d980d708e3
Aayush3217 <Aayushpal3217@gmail.com>
2023-07-28 20:23:35 +0530

    main

--- class Assigment.java
+++ class Assigment.java
@@ -22,3 +22,29 @@ class Assigment {
 
     }
 }
+
+//This code is purpose (Git demo):
+
+class Assigment {
+    public static void main(String[] args) {
+        int m1, m2, m3, m4;
+        m1 = m2 = m3 = m4 = 18;
+        System.out.println(m1 + " " + m2 + " " + m3 + " " + m4);
+        
+        int a=17;
+        a +=5;
+        System.out.println(a);
+
+        int b=18;
+        b -=4;
+        System.out.println(b);
+
+        double c=18;
+        c /=4;
+        System.out.println(c);
+
+        float d=17;
+        d *=2;
+        System.out.println(d);
+
+    }
