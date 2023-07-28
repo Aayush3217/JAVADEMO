831603f5274b1c943a3806216945d3503bb50b9f
Aayush3217 <Aayushpal3217@gmail.com>
2023-07-28 19:45:18 +0530

    main

---
+++ class Launch1.java
@@ -0,0 +1,16 @@
+class Launch1
+{
+    public static void main(String[] args) 
+    {
+     int num1=10;
+     int num2=20;
+     System.out.println(num1+num2);
+     System.out.println(num2-num1);
+     System.out.println(num1*num2);
+     System.out.println(num2/num1);
+     System.out.println(num2%num1);
+     System.out.println(40%7);
+     System.out.println(2%10);
+
+    }
+}
